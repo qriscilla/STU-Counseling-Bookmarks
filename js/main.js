1700240e01f56602102ae63f272a7ecc1e847470
qriscilla <pri.whang@gmail.com>
2020-01-13 21:14:58 -0500

    Iframe replaces front page images when link is active

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Side menu function
 (function($) {
 	"use strict";
 	var fullHeight = function() {
@@ -12,56 +13,15 @@
   });
 })(jQuery);
 
-
-
-
-
-
-
-
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
-
-
+// Slideshow on the front page
 var slideIndex = 1;
 showDivs(slideIndex);
-
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
-
 function currentDiv(n) {
   showDivs(slideIndex = n);
 }
-
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -77,3 +37,9 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
 }
+
+// Replacing slideshow with iframe
+$(".link").click(function() {
+  $("#images").replaceWith( $("#iframe") );
+  $("#iframe").show();
+});
