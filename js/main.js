a64f2f770f43ae25cb7456c408b8d297aec90b35
qriscilla <pri.whang@gmail.com>
2020-01-14 21:14:01 -0500

    Replaced the front page as requested

--- js/main.js
+++ js/main.js
@@ -14,32 +14,32 @@
 })(jQuery);
 
 // Slideshow on the front page
-var slideIndex = 1;
-showDivs(slideIndex);
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
+// var slideIndex = 1;
+// showDivs(slideIndex);
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+// function currentDiv(n) {
+//   showDivs(slideIndex = n);
+// }
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("demo");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" w3-white", "");
+//   }
+//   x[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " w3-white";
+// }
 
 // Replacing slideshow with iframe
-$(".link").click(function() {
-  $("#images").replaceWith( $("#iframe") );
-  $("#iframe").show();
-});
+// $(".link").click(function() {
+//   $("#images").replaceWith( $("#iframe") );
+//   $("#iframe").show();
+// });
